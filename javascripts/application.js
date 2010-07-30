9e6fd5d5e1749b42249fde2ba01386d4b2ecbfbf
Daniel Marino <starzonmyarmz@gmail.com>
2010-07-29 21:53:46 -0400

    Lots of changes due to changing the direction of this project. Not going to focus on preferences, localstorage for multiple dates, or other fancy pancy features. I added a function to switch the title input to textarea and vice versa based on screen size.

--- javascripts/application.js
+++ javascripts/application.js
@@ -6,13 +6,26 @@
  *
  */
 
-/*global window, document, $ */
+/*global window, document, localStorage $ */
 
 $(document).ready(function() {
 
+    // Create some markup
+    (function incrementMarkup() {
+        var td = $(".task_increment").html(), html = "";
+        for (var i = 0; i < 11; i++) {
+            html += '<td id="tc_' + i + '" class="task_increment">' + td + '</td>';
+        }
+        $(".task_increment").replaceWith(html);
+    }());
+
+    // Load data from localStorage if record exists
+    (function loadData() {
+
+    }());
 
     // Actions taking place when increment box is checked/unchecked
-    (function increment_checkbox() {
+    (function incrementCheckbox() {
         $(".increment label").live("click", function() {
 
             // Add/removes .active class and checks/unchecks input checkbox
@@ -29,10 +42,14 @@ $(document).ready(function() {
                 day_total  = $("body").find(".active").length * 0.25;
 
             // Updates task total
-            $(parent).find("span").text(task_total);
+            $(parent).find(".task_time_total").text(task_total);
 
             // Updates day total
             $("#day_time_total").text(day_total);
+
+            // Store data via localStorage
+            var currentData = $("#tasks_form").serializeArray();
+            localStorage.taskData = JSON.stringify(currentData);
 
         });
     }());
@@ -59,20 +76,29 @@ $(document).ready(function() {
         input.live("focus", function() {
             $(this).removeClass("has_value");
         });
-
     }());
 
-    // Return false if form is submitted - otherwise data is erased
-    (function noSubmit() {
-        $("form").submit(function() {
-            return false;
+    // Switch task title input to textarea for mobile devices
+    (function inputToTextarea() {
+        $(window).bind("resize load", function() {
+            var input = $(".task_title input, .task_title textarea");
+            input.each(function() {
+                var val  = $(this).val(),
+                    name = $(this).attr("name"),
+                    id   = $(this).attr("id");
+                    atts = 'placeholder="Edit task title" autocomplete="off"';
+                if ($(window).width() < 800) {
+                    $(this).replaceWith('<textarea name="' + name + '" id="' + id + '"' + atts + '>' + val + '</textarea>');
+                } else {
+                    $(this).replaceWith('<input name="' + name + '" id="' + id + '"' + atts +'value="' + val + '" />');
+                }
+            });
         });
     }());
 
-    // Show/hide nav items
-    (function toggleNavItems() {
-        $("#toggle_prefs").click(function() {
-            $("#prefs").toggle()
+    // Return false if form is submitted - otherwise data will be erased
+    (function noSubmit() {
+        $("form").submit(function() {
             return false;
         });
     }());
