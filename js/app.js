597c1e416a1f5e703da2e5e47fffdb275e6ee9cf
Daniel Marino <starzonmyarmz@gmail.com>
2012-03-02 15:56:31 -0500

    adding click functionality on the time increment bubbles

--- js/app.js
+++ js/app.js
@@ -11,6 +11,10 @@ $(function(){
             };
         },
 
+        /* 44 is equal to 11 hours split into 15 minute increments.
+         * We set all the increments to false by default because the
+         * increment is set to true when clicked on
+         */
         setTime: function() {
             var time = [];
             for (var x = 0; x < 45; x++) {
@@ -28,14 +32,21 @@ $(function(){
     var tasks = new TaskCollection();
 
     var TimeIncrement = Backbone.View.extend({
-
         tagName: 'a',
         className: 'increment',
+        initialize: function() {
+
+        },
+        events: {
+            //"click": "toggleIncrement"
+        },
         render: function() {
             $(this.el).text(this.options.inc.toString());
             return this;
+        },
+        toggleIncrement: function(inc) {
+            console.log(this)
         }
-
     });
 
     var TaskView = Backbone.View.extend({
@@ -45,7 +56,8 @@ $(function(){
 
         events: {
             "keyup .title": "saveTask",
-            "click .remove": "removeTask"
+            "click .remove": "removeTask",
+            "click .increment": "testFn"
         },
 
         render: function() {
@@ -58,18 +70,13 @@ $(function(){
             var title = this.model.get("title");
             this.$('.title').val(title);
 
+            var increments = [];
+
             _.each(this.model.get("time"), function(increment) {
-                console.log( new TimeIncrement({ inc: increment }).render().el )
+                increments.push(new TimeIncrement({ inc: increment }).render().el);
             });
 
-            //this.$('.time').append(time);
-
-            // See https://github.com/cramerdev/RidleyClient/blob/master/js/views/search/field.js#L67
-            //var view = new TimeIncrement({ model: task });
-            //$('.tasks').append(view.render().el);
-
-
-
+            this.$('.time').html(increments);
         },
 
         saveTask: function() {
@@ -79,6 +86,10 @@ $(function(){
         removeTask: function() {
             this.model.destroy();
             $(this.el).remove();
+        },
+        testFn: function(t) {
+            debugger
+            //console.log(this.model.get("time"))
         }
     });
 
